0bdd4b80f42b6f9136ecbdcf26cdeb9d07da0187
Dust <world3nd@163.com>
2016-10-20 16:53:26 +0800

    incremental encryption

--- Demo.py
+++ Demo.py
@@ -3,6 +3,9 @@
 import os
 from Encryption import AESCipher, b64encode, b64decode, random_pwd, RSAEncryption
 
+# 自行定义换行符 or 回车符
+LINE_BREAK = '\n'
+
 
 def get_files_list(ext):
     files_list = os.listdir(os.getcwd())
@@ -29,10 +32,73 @@ def aes_encrypt(pwd):
     aes = AESCipher(pwd)
     for txt_file in get_files_list('txt'):
         with open(txt_file, 'r', encoding='utf8') as f:
-            contents = [b64encode(aes.encrypt_line(line)) + '\n' for line in f]
+            contents = [b64encode(aes.encrypt_line(line)) + LINE_BREAK for line in f]
         with open(txt_file, 'w', encoding='utf8') as f:
             f.writelines(contents)
         print('{} 加密完成'.format(txt_file))
+
+
+def incremental_aes_encrypt(pwd, mark='*'):
+    """
+    增量加密。
+    :param pwd: 密码
+    :param mark: 区分加密与否的标识
+                 该标识应在文件中独占一行，表示在其上的文本行均已加密，其下的文本行均未加密
+                 注意标识不应有与 base64 编码后的密文相同的可能性
+    """
+    aes = AESCipher(pwd)
+    for txt_file in get_files_list('txt'):
+        with open(txt_file, 'r', encoding='utf8') as f:
+            contents = f.readlines()
+        encrypt_contents = encrypt_new_contents(aes, contents, mark)
+        if encrypt_contents is False:
+            # 跳过无标识的文件
+            print('{} 未找到标识'.format(txt_file))
+            continue
+
+        with open(txt_file, 'w', encoding='utf8') as f:
+            f.writelines(encrypt_contents)
+            # 追加加密标识行
+            f.writelines(mark)
+        print('{} 加密完成'.format(txt_file))
+
+
+def encrypt_new_contents(aes, contents, mark):
+    """
+    查找并加密增加的新内容，如果未找到标志符，返回 False
+    如果想要加密全新的文件，可以更改此函数，或手动在新文件首行加入 mark
+    注意，在 Windows 记事本中编辑 utf8 文本后会自动在文件开头加上 BOM，将导致识别 mark 失败，解决方法很多，就不细说了
+    """
+    for i, line in enumerate(contents):
+        if line.startswith(mark):
+            encrypt_contents = contents[:i]
+            new_contents = [b64encode(aes.encrypt_line(l)) + LINE_BREAK for l in contents[i + 1:]]
+            encrypt_contents.extend(new_contents)
+            return encrypt_contents
+    return False
+
+
+def incremental_aes_decrypt(pwd, mark='*'):
+    """
+    针对增量加密的全文解密。
+    通常情况下没必要频繁解密之前已加密的内容，否则该使用场景并不适宜用增量加密。
+    """
+    aes = AESCipher(pwd)
+    for txt_file in get_files_list('txt'):
+        with open(txt_file, 'r', encoding='utf8') as f:
+            contents = f.readlines()
+        for i, line in enumerate(contents):
+            if line.startswith(mark):
+                decrypt_contents = [aes.decrypt_line(b64decode(line)).decode() for line in contents[:i]]
+                decrypt_contents.extend(contents[i + 1:])
+                break
+        else:
+            continue
+
+        with open(txt_file, 'w', encoding='utf8') as f:
+            f.writelines('*' + LINE_BREAK)
+            f.writelines(decrypt_contents)
+        print('{} 解密完成'.format(txt_file))
 
 
 def demo_1():
@@ -72,11 +138,22 @@ def demo_2():
     # aes_decrypt(password)
 
 
+def demo_3():
+    password = random_pwd(32)
+    print(password)
+    # 增量加密
+    incremental_aes_encrypt(password)
+    # 对增量加密的全文解密
+    incremental_aes_decrypt(password)
+
+
 def demos():
     # 1、AES 加密文本
     # demo_1()
     # 2、RSA 加密 AES 密码，AES 加密文本
     # demo_2()
+    # 3、AES 增量加密、解密
+    # demo_3()
     pass
 
 
